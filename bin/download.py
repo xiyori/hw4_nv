8d4bcb3c05c4a7e9998a50a2853f2c218b1cb139
xiyori <foma@shipilov.ru>
2022-12-21 18:49:42 +0300

    Update download.py

--- bin/download.py
+++ bin/download.py
@@ -11,28 +11,26 @@ from src import config
 parser = argparse.ArgumentParser(description=".")
 parser.add_argument("-c", "--config", metavar="CONFIG", type=str,
                     required=True, help="Config filename.")
-parser.add_argument("-d", "--data_only", action="store_true",
-                    help="Donwload data only.")
+parser.add_argument("-d", "--datasphere", action="store_true",
+                    help="Do not move downloaded data to dataset dir.")
 args = parser.parse_args()
 
 config_ = getattr(config, args.config)
 
 env = os.environ.copy()
-if "datasphere" in args.config:
-    env["PATH"] += ":/home/jupyter/.local/bin"
 
 data_config = config_.DataConfig()
-data_dir = os.path.abspath(data_config.data_dir)
+data_dir = data_config.data_dir
 
-if args.data_only:
-    process = ["bin/download.sh", data_dir]
-else:
-    checkpoint_path = os.path.abspath("./resources/chkpoints")
-    results_path = os.path.abspath("./resources/results")
-    os.makedirs(checkpoint_path, exist_ok=True)
-    os.makedirs(results_path, exist_ok=True)
+if args.datasphere:
+    env["PATH"] += ":/home/jupyter/.local/bin"
+    data_dir = "./resources"
 
-    process = ["bin/download.sh", data_dir,
-               checkpoint_path, results_path]
+data_dir = os.path.abspath(data_dir)
+checkpoint_path = os.path.abspath("./resources/chkpoints")
+results_path = os.path.abspath("./resources/results")
+os.makedirs(checkpoint_path, exist_ok=True)
+os.makedirs(results_path, exist_ok=True)
 
-subprocess.run(process, env=env)
+subprocess.run(["bin/download.sh", data_dir,
+                checkpoint_path, results_path], env=env)
