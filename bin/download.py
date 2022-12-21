f458cfef122593ab367aee823f1bd80cce997e94
xiyori <foma@shipilov.ru>
2022-12-21 18:39:07 +0300

    Data only mode

--- bin/download.py
+++ bin/download.py
@@ -11,6 +11,8 @@ from src import config
 parser = argparse.ArgumentParser(description=".")
 parser.add_argument("-c", "--config", metavar="CONFIG", type=str,
                     required=True, help="Config filename.")
+parser.add_argument("-d", "--data_only", action="store_true",
+                    help="Donwload data only.")
 args = parser.parse_args()
 
 config_ = getattr(config, args.config)
@@ -21,12 +23,16 @@ if "datasphere" in args.config:
 
 data_config = config_.DataConfig()
 data_dir = os.path.abspath(data_config.data_dir)
-checkpoint_path = os.path.abspath("./resources/chkpoints")
-results_path = os.path.abspath("./resources/results")
 
-os.makedirs(checkpoint_path, exist_ok=True)
-os.makedirs(results_path, exist_ok=True)
-os.chdir("./resources")
+if args.data_only:
+    process = ["bin/download.sh", data_dir]
+else:
+    checkpoint_path = os.path.abspath("./resources/chkpoints")
+    results_path = os.path.abspath("./resources/results")
+    os.makedirs(checkpoint_path, exist_ok=True)
+    os.makedirs(results_path, exist_ok=True)
 
-subprocess.run(["../bin/download.sh", data_dir,
-                checkpoint_path, results_path], env=env)
+    process = ["bin/download.sh", data_dir,
+               checkpoint_path, results_path]
+
+subprocess.run(process, env=env)
