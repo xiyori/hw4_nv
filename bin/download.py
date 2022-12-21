da14946f88bca00d19c7b756a22fce10d6530c7d
xiyori <foma@shipilov.ru>
2022-12-21 18:32:40 +0300

    Fix legacy code

--- bin/download.py
+++ bin/download.py
@@ -19,11 +19,12 @@ env = os.environ.copy()
 if "datasphere" in args.config:
     env["PATH"] += ":/home/jupyter/.local/bin"
 
-train_config = config_.TrainConfig()
-data_dir = os.path.abspath(train_config.data_dir)
+data_config = config_.DataConfig()
+data_dir = os.path.abspath(data_config.data_dir)
 checkpoint_path = os.path.abspath("./resources/chkpoints")
 results_path = os.path.abspath("./resources/results")
 
+os.makedirs(checkpoint_path, exist_ok=True)
 os.makedirs(results_path, exist_ok=True)
 os.chdir("./resources")
 
