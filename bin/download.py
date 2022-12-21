dfc1f01b91e87f26c7ed73ef3a6c8682d0922ca4
xiyori <foma@shipilov.ru>
2022-12-21 22:32:50 +0300

    Update download.py

--- bin/download.py
+++ bin/download.py
@@ -24,13 +24,14 @@ data_dir = data_config.data_dir
 
 if args.datasphere:
     env["PATH"] += ":/home/jupyter/.local/bin"
-    data_dir = "./resources"
+    data_dir = "./resources/" + os.path.basename(data_dir)
 
 data_dir = os.path.abspath(data_dir)
 checkpoint_path = os.path.abspath("./resources/chkpoints")
-results_path = os.path.abspath("./resources/results")
+results_path = os.path.abspath("./resources/predicted")
 os.makedirs(checkpoint_path, exist_ok=True)
 os.makedirs(results_path, exist_ok=True)
+os.chdir("./resources")
 
 subprocess.run(["bin/download.sh", data_dir,
                 checkpoint_path, results_path], env=env)
